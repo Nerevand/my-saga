dfdca12a0dc56cf341285322123b656155d45921
Nerevand <kruchkovskiy2@gmail.com>
2019-08-14 16:19:04 +0300

    change post to pus

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,15 +1,15 @@
 import { put, call, takeEvery } from 'redux-saga/effects'
 import axios from 'axios';
 
-const createBlaBla = (payload) => {
-    return axios.post('https://jsonplaceholder.typicode.com/posts', payload)
-      .then(response => response.data)
+const requestToServer = (payload) => {
+    return axios.put('https://jsonplaceholder.typicode.com/posts/11', payload)      //modify post with id=11
+      .then(response => response)
       .catch(err => err);
   }
 
 export function* incrementAsync(obj) {       //here we have obj(from ../App.js)
     console.log("action", obj);
-    const data = yield call(createBlaBla, { mail: obj.mail, age: 100 }); //call our axios function
+    const data = yield call(requestToServer, {  title: 'new text here' }); //call our axios function
     console.log("data", data);      
     yield put({ type: 'INCREMENT', payload: data });        //write our data to redux store
 }
