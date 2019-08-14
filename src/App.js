af7932f5eaa30ca369ba3d4cd3f598699a4bcb9f
Nerevand <kruchkovskiy2@gmail.com>
2019-08-14 15:19:54 +0300

    change comment

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import { bindActionCreators } from 'redux'
 
 import logo from './logo.svg';
 import './App.css';
-import { onIncrement } from './actions';        //import action then watch line 40
+import { onIncrement } from './actions';        //import action then watch line 39
 
 function App(props) {
   const handleClick = () => {
     const { onIncrement } = props;          //now our action is in props, get it 
     onIncrement({mail: 'mail@gmail.com'});  //and call with params(if needed)
+    document.getElementById('text').innerHTML='next step: visit console';
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <p id='text'></p>
         <div
           className="App-link"
           onClick={handleClick}       //call our function with action, then line 11
         >
-          Learn React
+          click me
         </div>
       </header>
     </div>
@@ -37,7 +36,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  onIncrement         //added our action to bindActionCreators, then line 24
+  onIncrement         //added our action to bindActionCreators, then line 23
 }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
